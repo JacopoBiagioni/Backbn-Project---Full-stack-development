bc653e7754577e62190da79b1cace4e7b9c129ba
JacopoBiagioni <biagioni.jacopo@iisgalvanimi.edu.it>
2024-05-22 11:57:26 +0000

    mongo err

--- server/app.py
+++ server/app.py
@@ -1,8 +1,36 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+#Creo una funzione che posso riciclare ogni volta che devo accedere al DB
+def get_db():
+    client = MongoClient(host='test_mongodb',
+                         port=27017, 
+                         username=os.environ["MONGO_INITDB_ROOT_USERNAME"], 
+                         password=os.environ["MONGO_INITDB_ROOT_PASSWORD"],
+                        authSource="admin")
+    db = client[os.environ["MONGO_INITDB_DATABASE"]]
+    return db
+
+#Creo una route per ottenere tutti gli animali
+@app.route('/animals')
+def get_stored_animals():
+    db=""
+    try:
+        db = get_db()
+        _animals = db.animal_tb.find()
+        animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
+        return jsonify({"animals": animals})
+    except:
+        pass
+    finally:
+        if type(db)==MongoClient:
+            db.close()
+
 
 @app.route('/')
 def ping_server():
@@ -11,6 +39,15 @@ def ping_server():
 @app.route('/simple_json')
 def simple_json():
     return jsonify('{nome : jacopo}')
+
+@app.route('/environment')
+def env():
+    return jsonify(
+    {"env":[
+        {"MONGO_INITDB_DATABASE" : os.environ["MONGO_INITDB_DATABASE"]},
+        {"MONGO_INITDB_ROOT_USERNAME" : os.environ["MONGO_INITDB_ROOT_USERNAME"]},
+        {"MONGO_INITDB_ROOT_PASSWORD" : os.environ["MONGO_INITDB_ROOT_PASSWORD"]}
+    ]})
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
